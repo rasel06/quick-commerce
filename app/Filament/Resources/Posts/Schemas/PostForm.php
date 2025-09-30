a3fb39873382cbfe251c6eaf52b08cb334b2c497
rasel06 <rasel06@gmail.com>
2025-09-30 19:03:22 +0600

    Markdown Editor

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Posts\Schemas;
 
 use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
@@ -17,7 +18,7 @@ class PostForm
                 TextInput::make('title')
                     ->required()
                     ->maxLength(255),
-                Textarea::make('content')
+                MarkdownEditor::make('content')
                     ->required()
                     ->maxLength(65535),
                 Checkbox::make('is_published')
